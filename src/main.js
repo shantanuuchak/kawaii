80426a2c416a5005f864680fc0f2456951bd7348
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-09-22 12:58:31 +0000

    file with images

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
+import { chars, charsData } from "./characters";
+
 // Targetting Elements
 const gameGrid = document.querySelector(".game-grid");
 
+// Game Variables
+const virtualGrid = [];
+
 function getRandChar() {
-  return characters[Math.floor(Math.random() * characters.length)];
+  return chars[Math.floor(Math.random() * chars.length)];
 }
 
 function buildGrid() {
   for (let i of Array(36).fill(1)) {
+    // Creating box and image elements
     const box = document.createElement("div");
+    const img = document.createElement("img");
+    img.src = charsData[getRandChar()];
+
+    // Adding image inside the box
+    box.appendChild(img);
+    // Adding the box in grid
     box.classList.add("box");
     gameGrid.appendChild(box);
   }
